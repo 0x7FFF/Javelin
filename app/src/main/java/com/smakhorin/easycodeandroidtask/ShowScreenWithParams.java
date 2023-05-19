69e200f776e392b9ac742c29dd49240c344fe47d
Makhorin <s.makhorin@arammeem.com>
2023-05-19 14:51:53 +0300

    Remove unused interface.

--- app/src/main/java/com/smakhorin/easycodeandroidtask/ShowScreenWithParams.java
+++
@@ -1,8 +0,0 @@
-package com.smakhorin.easycodeandroidtask;
-
-import androidx.annotation.IdRes;
-import androidx.fragment.app.FragmentManager;
-
-public interface ShowScreenWithParams {
-    void show(@IdRes Integer containerId, FragmentManager fragmentManager, Object... params);
-}
